e622b7a267f52ba306b34013bbce31f461a37551
yoonhoShin <amuse1991@gmail.com>
2019-04-27 07:06:04 +0900

    create project

---
+++ src/main/java/io/github/amuse/sims_server_spring/SimsServerSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.amuse.sims_server_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimsServerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimsServerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/amuse/sims_server_spring/SimsServerSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.amuse.sims_server_spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimsServerSpringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
